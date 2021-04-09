0de819fdb276bb413945a528608caccbe051a058
Lymina <74780025+Lymina@users.noreply.github.com>
2021-04-09 19:01:47 +0300

    Update config.py

--- dis_bot/config.py
+++ dis_bot/config.py
@@ -1,7 +1,7 @@
 """зашифрованные данные, ибо дискорд не может работать со слитыми на всеобщее обозрение данными ;Р"""
 
 settings = {
-    'token': 'T0RJNE56TXlOalkxTmpFMU9EYzJNVEUzLllHdDNjQS4zWl8wZVBRbzhzZGgxaW1xUWw0UGtBXzdpU3c=',
+    'token': 'сикрет'
     'bot': 'MzkyZjk1ODNjMWZiOTBmYmE0NmYwZTU2N2MwY2QyMGIzZWNhMGVhY2QxNTFjN2I5OWM3ZjhlMTc4ZjQxMmMxNQ==',
     'client_id': 'ODI4NzMyNjY1NjE1ODc2MTE3'
 }
