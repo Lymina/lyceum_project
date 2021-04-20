0aa1a767b3fea01cf94cfb2c3f698c110a411e74
SarpinskayaAnastasia <74780246+SarpinskayaAnastasia@users.noreply.github.com>
2021-04-20 17:45:09 +0300

    добавлена возможность занесения игроков
    ...но пока это черновой вариант

--- dis_bot/main_bot.py
+++ dis_bot/main_bot.py
@@ -1,23 +1,26 @@
 import discord
 import asyncio
-from discord.ext  import commands
+from discord.ext import commands
 import random
 import json
-import requests 
+import requests
 from translate import Translator
-import lists_roles
+from lists_roles import *
 import tracemalloc
+import sqlite3
 
 tracemalloc.start()
 
 TOKEN = 'тут мог бы быть токен'
 MAIN_CHANNEL = 0  # id главного канала
 TOWN_N_CHANNEL = 0  # id города-n
+DB = sqlite3.connect("db/roles_players.sqlite")
 
 # чтобы "активировать" команду пользователь должен написать ">" перед сообщением
 bot = commands.Bot(command_prefix='>')
 client = discord.Client()
-town_n_channel = bot.get_channel(TOWN_N_CHANNEL) # определяем канал по id
+town_n_channel = bot.get_channel(TOWN_N_CHANNEL)  # определяем канал по id
+
 
 # в булочную через Китай, здесь обитают все переменные (да, проще было сделать нельзя)
 class ToTheBakeryThroughСhina:
@@ -25,6 +28,7 @@ class ToTheBakeryThroughСhina:
         self.flag_new_game = 0  # игра ещё не началась, игроки только набираются, можно брать роль
         self.flag_now_game = 0  # игра идёт прямо сейчас
         self.list_gamers = []  # список игрков
+
 
 help_everything = ToTheBakeryThroughСhina()
 
@@ -34,10 +38,6 @@ def color_str(text):  # делает текст жирным
 
 
 def get_quote():  # функция для получения мотивирующих цитат через api
-    response = requests.get("https://zenquotes.io/api/random")
-    json_data = json.loads(response.text)
-    quote = json_data[0]['q'] + "\n -" + json_data[0]['a']
-    return (quote)
     response = requests.get("https://zenquotes.io/api/random")
     json_data = json.loads(response.text)
     quote = json_data[0]['q'] + "\n -" + json_data[0]['a']
@@ -52,33 +52,28 @@ def translate(text_to_translate):  # функция перевода цитат 
 
 async def rule_for_play():  # для игры, когда все роли созданы и игра только началась
     await bot.wait_until_ready()
-
-    town_n_channel = bot.get_channel(TOWN_N_CHANNEL) # определяем нужный нам чат по id
+    town_n_channel = bot.get_channel(TOWN_N_CHANNEL)  # определяем нужный нам чат по id
     # отправляем сообщения
-    await town_n_channel.send('Итак, все роли изданы, а мафиози познакомились! Мы начинаем!')
-
-    await town_n_channel.send('```Уважаемые жители, у меня для вас ужасные новости! В городе завелась мафия!!'
-                              'Ваша цель истребить заразу и снова сделать город безопасным.'
-                              'Цель мафии подчнить себе город, истребив жалких жителей.'
-                              'Наша игра будет состоять из 2 периодов: день и ночь. Ночью активные роли совершают свои'\
-                              'действия, а днём мы все дружно решаеим кого повесить ^w^'
-                              'Все всё поняли? Отлично! Мы начинаем (⌐■_■)'
-                              '```')
+    await town_n_channel.send('Итак, все роли разданы, а мафиози познакомились! Мы начинаем!')
+    await town_n_channel.send('''```Уважаемые жители, у меня для вас ужасные новости! В городе завелась мафия!!! Ваша 
+    цель истребить заразу и снова сделать город безопасным. Цель мафии подчнить себе город, истребив жалких жителей. 
+    Наша игра будет состоять из 2 периодов: день и ночь. Ночью активные роли совершают свои действия, а днём мы все 
+    дружно решаеим кого повесить ^w^ Все всё поняли? Отлично! Мы начинаем (⌐■_■)```''')
 
 
-def distribution(members): # функция раскидки ролей
+def distribution(members):  # функция раскидки ролей
     length = len(members)
     random.shuffle(members)
     random.shuffle(members)
     random.shuffle(members)
-    random.shuffle(members) # чтоб уж наверняка :D
+    random.shuffle(members)  # чтоб уж наверняка :D
+    # минимальное кол-во 5
     if length < 5:
         return False, 'Увы, недостаточно игроков.'
-
     # если всё получается
     elif length == 5:
-        bot.loop.create_task(rule_for_play()) # выводим сообщение, что игра началась
-        return True, zip(members, FIVE_MEMBERS) # возвращаем, что всё хорошо и список списков из человек-роль
+        bot.loop.create_task(rule_for_play())  # выводим сообщение, что игра началась
+        return True, zip(members, FIVE_MEMBERS)  # возвращаем, что всё хорошо и список списков из человек-роль
     # и так для каждого кол-ва игроков, для всех разные сценарии
     elif length == 6:
         bot.loop.create_task(rule_for_play())
@@ -104,9 +99,23 @@ def distribution(members): # функция раскидки ролей
     elif length == 13:
         bot.loop.create_task(rule_for_play())
         return True, zip(members, THIRTEEN_MEMBERS)
-    # максимальное кол-во игоров 13, минимальное 5
+    # максимальное кол-во игоров 13
     else:
-        return False, 'Слишком много игроков, будет балаган ._.'
+        return False, 'Слишком много игроков, будет балаган ._."'
+
+
+def transfer_players(distribution_tuple):  # используется в паре с вышенаписанной функцией
+    if distribution_tuple[0]:
+        for mem_num in distribution_tuple[1]:  # mem_num - кортеж, т.к. второй элемент входного кортежа - это список
+            # кортежей, который возвращает функция zip
+            cur = DB.cursor()
+            cur.execute(f"""INSERT INTO players(id, nick_name, nick_id, role)
+            VALUES({distribution_tuple[1].index(mem_num) + 1}, {mem_num[0]}, {mem_num[1]}, {mem_num[1]})""").fetchall()
+            # я не уверена в типе данных игрока, то ли это строка, то ли что-то другое... оставлю пока так
+    else:
+        await bot.wait_until_ready()
+        main_channel = bot.get_channel(MAIN_CHANNEL)  # определяем нужный нам чат по id
+        await main_channel.send(f'{distribution_tuple[1]}')
 
 
 @bot.command()  # помогалка, тут всё понятно
@@ -119,7 +128,6 @@ async def help_me(ctx):
     (чтобы писать в канале "город-n") или "Наблюдатель" (чтобы следить за игрой в канале "город-n") (>ω^)
     * {color_str(">start_game")} - Все игроки готовы? Отлично, введи эту команду и мы начнём!  
     ᄽὁȍ ̪ őὀᄿ
-
     * {color_str('>i_am_sad')} - Тебе грустно? Ничего страшного, я выведу
      специально для тебя мотивирующую цитату   (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧''')
 
@@ -128,8 +136,7 @@ async def help_me(ctx):
 async def new_game(ctx):
     help_everything.flag_new_game = 1
     # как вариант для отметки автора сообщения ctx.message.author.mention(c @) или ctx.message.author (без @)
-    await ctx.send(f'''Игрок {ctx.message.author.mention} объявил набор в новую игру! Комната ожидания открыта! 
-
+    await ctx.send(f'''Игрок {ctx.message.author.mention} объявил набор в новую игру! Комната ожидания открыта!
     \nВсех желающих присоединиться к игре просим взять у нашего бота роль "Игрок",\
      те кто хотят просто посмотреть за ходом игры пусть возьмут роль "Наблюдатель". 
      Для этого напишите команду (>give @"пользователь" @"роль")
@@ -139,11 +146,10 @@ async def new_game(ctx):
 @bot.command()  # начало игры(!), раскидываем персонажей между игроками, роли брять нельзя
 async def start_game(ctx):
     help_everything.flag_new_game = 0  # больше роли брать нельзя!
-    help_everything.flag_now_gane = 1  # игра началась
-    await ctx.send(
-        f'Комната ожидания закрыта! Кто не успел, тот опоздал! Роли брать нельзя до следующей игровой сессии!'
-        f'Игроки, когда увидете в канале "город-n" сообщение можете начинать!'
-        f'Наблюдатели, следите, чтобы всё было честно!')
+    help_everything.flag_now_game = 1  # игра началась
+    await ctx.send('''Комната ожидания закрыта! Кто не успел, тот опоздал! Роли брать нельзя до следующей игровой 
+    сессии! Игроки, когда увидете в канале "город-n" сообщение можете начинать! Наблюдатели, следите, чтобы всё было 
+    честно!''')
     distribution(help_everything.list_gamers)  # начинаем раскидку ролей
 
 
@@ -156,12 +162,8 @@ async def citata(ctx):
 
 @bot.command()  # раздаёт роли игркам и заносит их в свой список (решить проблему с выводом из списка)
 async def give(ctx, member: discord.Member, role: discord.Role):
-    if help_everything.flag_new_game == 1:  # делаем проверку, что комната ожидания открыта и роли брать можно
+    if help_everything.flag_new_game:  # делаем проверку, что комната ожидания открыта и роли брать можно
         try:
-            getrole = discord.utils.get(ctx.guild.roles, id=role.id)
-            await member.add_roles(getrole)  # выдаём роль
-            help_everything.spis_gamers.append(member)  # добавляем пользователя в список
-            await ctx.send(f'{member} получил\а роль {role}')  # говорим, что всё получилось
             get_role = discord.utils.get(ctx.guild.roles, id=role.id)
             await member.add_roles(get_role)  # выдаём роль
             help_everything.list_gamers.append(member)  # добавляем пользователя в список
@@ -173,16 +175,11 @@ async def give(ctx, member: discord.Member, role: discord.Role):
         # если что-то пошло не так - говорим об этом чату
         await ctx.send(f'''{ctx.message.author}, рано брать роли мы ещё не начали новую игру!
         \nДля того чтобы начать новую игру введи команду ">new_game"''')
-        await ctx.send(f'''{ctx.message.author}, рано брать роли, мы ещё не начали новую игру!
-        \nДля того, чтобы начать новую игру введи команду ">new_game"''')
 
 
 def stop_game():  # завершение сессии игры, обнуляем всё
     # вставить обнуление роли Игрок(!)
-    help_flag.flag_game_now = 0  # игра закончилась, опускаем флаг
-    help_everything.spis_gamers = []  # опустошаем список, игроки расходятся
-    help_flag.flag_game_now = 0  # игра закончилась, опускаем флаг  # итак, сейчас я буду ругаться...
-    # ГДЕ НАХОДИТСЯ ЭТА ПЕРЕМЕННАЯ
+    help_everything.flag_game_now = 0  # игра закончилась, опускаем флаг
     help_everything.list_gamers = []  # опустошаем список, игроки расходятся
 
 
